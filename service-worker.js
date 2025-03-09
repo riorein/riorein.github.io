14932495b9f857f0be0a819a0f6e333eceacbc64
riorein <panafutin2@gmail.com>
2025-03-09 17:50:01 +0300

    Update Service Worker for modules

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,39 @@
-const CACHE_NAME = 'prize-calculator-v1';
-const ASSETS = [
-  '/', // Основной HTML-файл
-  '/index.html', // Если приложение находится в корне
-  '/styles.css', // Если у вас отдельный файл стилей
-  '/icon.png' // Иконка приложения
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS))
+    caches.open('v2').then(function(cache) {  // Изменили версию на 'v2'
+      return cache.addAll([
+        './index.html',
+        './manifest.json',
+        './icon.png',
+        './validation.js',
+        './calculator.js',
+        './ui.js'
+      ]).catch(function(error) {
+        console.log('Ошибка кэширования:', error);  // Логируем ошибки
+      });
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Очистка старого кэша
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== 'v2';
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
   );
 });
